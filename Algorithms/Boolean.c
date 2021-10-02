7de52ccf16333a0f5ed84cb63d6c7109fb181738
Marcos <30581037+marcosvlt@users.noreply.github.com>
2021-10-02 20:52:13 -0300

    Create Boolean.c

---
+++ Algorithms/Boolean.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+In C cannot declare boolean
+0 = Is false
+!0 = True
+*/
+
+int main() {
+
+	int num = 1;
+	
+	
+	if(num){
+		printf("True");
+	}
+	else{
+		printf("False");
+	}
+				
+	}
